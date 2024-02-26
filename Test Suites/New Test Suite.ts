d9890e138339eef571f3065a2ab7c05b46d69d57
Duy Luong <duy.luong@katalon.com>
2024-02-26 08:46:58 +0100

    Init

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1f7c72a6-821a-4adf-bfb5-a6e1fd031b3a</testSuiteGuid>
+   <testCaseLink>
+      <guid>0805c14f-d685-48b1-becd-087e5847e65c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>9274d2cd-3a7d-4c5c-8557-a4b29e47c2c4</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>9274d2cd-3a7d-4c5c-8557-a4b29e47c2c4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>demo</value>
+         <variableId>be30ec10-134c-49c3-858e-b83bdfb74118</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
